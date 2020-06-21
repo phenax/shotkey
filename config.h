900c5a3a955324aad6035c499003b16bbdd0d96c
Akshay Nair <phenax5@gmail.com>
2020-06-21 15:52:00 +0530

    Fixes style issues

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 // Shell
-static char shell[] = "/bin/sh";
+char shell[] = "/bin/sh";
 
 #define Super Mod4Mask
 
@@ -13,7 +13,7 @@ enum {
 
 // Define mode key bindings here
 // NOTE: "10" here is the maximum number of key bindings for each mode
-static Key modes[MODE_SIZE][10] = {
+Key modes[MODE_SIZE][10] = {
   [Music] = {
     { 0, XK_m,   cmd("notify-send inside_music") },
   },
@@ -24,17 +24,17 @@ static Key modes[MODE_SIZE][10] = {
 };
 
 // Define normal mode key bindings here
-static Key keys[] = {
+Key keys[] = {
   { Super|ShiftMask, XK_y,   cmd("notify-send hello") },
   { Super|ShiftMask, XK_z,   mode(Music, False) },
   { Super|ShiftMask, XK_x,   mode(Bright, True) },
 };
 
-static ModeProperties mode_properties[MODE_SIZE] = {
+ModeProperties mode_properties[MODE_SIZE] = {
   [Music] = { "Music player" },
   [Bright] = { "Brightness" },
 };
 
 // Call this script on mode change
-static char* on_mode_change = "notify-send \"kadj [$SHOTKEY_MODE_ID] $SHOTKEY_MODE_LABEL\"";
+char* on_mode_change = "notify-send \"kadj [$SHOTKEY_MODE_ID] $SHOTKEY_MODE_LABEL\"";
 
